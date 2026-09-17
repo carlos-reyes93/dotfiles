4835bb6fabab756c49f18b47320566ce8cc410fc
Carlos Reyes <contacto@carlosreyc.dev>
2026-09-17 10:30:45 -0600

    feat!: added new stuff via stow

--- nvim/lua/carlosreyc/plugins/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { import = 'carlosreyc.plugins.core' },
-  { import = 'carlosreyc.plugins.ui' },
-  { import = 'carlosreyc.plugins.coding' },
-  { import = 'carlosreyc.plugins.lang' },
-  -- { import = 'carlosreyc.plugins.extras' },
-}
